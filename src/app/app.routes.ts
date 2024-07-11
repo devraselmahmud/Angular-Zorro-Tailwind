6f38b9b17b983074680fdbbdf54996669d973cfe
rasel.mahmud <rasel.mahmud@bracits.com>
2024-07-11 16:37:10 +0600

    feat: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
   {path: 'content', loadChildren: () => import('./content/content.routes').then(m => m.routes)},
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.routes) }
+  { path: 'home', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.routes) }
 ];
